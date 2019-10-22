e457f3a1f25daaac67011c22b666c27b08022be3
makowalski <marcin.kowalski.it.consulting@gmail.com>
2019-10-22 16:34:02 +0100

    refactoring

--- bin/decrypt.js
+++ bin/decrypt.js
@@ -1,10 +1,11 @@
-var CryptoJS = require("crypto-js");
+var CryptoJS = require('crypto-js');
 var fs = require('fs');
 
-var content = fs.readFileSync('target/appeal/json/UserProfile.json', 'utf8');
+var fileName = 'target/appeal/json/UserProfile.json';
+var content = fs.readFileSync(fileName, 'utf8');
 
 // Decrypt
 var bytes  = CryptoJS.AES.decrypt(content.toString(), process.env.IA_CCD_SECRET_KEY);
 var plaintext = bytes.toString(CryptoJS.enc.Utf8);
 
-fs.writeFileSync('target/appeal/json/UserProfile.json', plaintext);
+fs.writeFileSync(fileName, plaintext);
--- bin/encrypt.js
+++ bin/encrypt.js
@@ -1,9 +1,10 @@
-var CryptoJS = require("crypto-js");
+var CryptoJS = require('crypto-js');
 var fs = require('fs');
 
-var content = fs.readFileSync('definitions/appeal/env/prod/UserProfile.json', 'utf8');
+var fileName = 'definitions/appeal/env/prod/UserProfile.json';
+var content = fs.readFileSync(fileName, 'utf8');
 
 // Encrypt
 var ciphertext = CryptoJS.AES.encrypt(content, process.env.IA_CCD_SECRET_KEY);
 
-fs.writeFileSync('definitions/appeal/env/prod/UserProfile.json', ciphertext);
+fs.writeFileSync(fileName, ciphertext);
