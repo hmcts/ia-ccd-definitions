81c56e98657f0b710e9ce292b30c947967209d19
david.crespo <david.crespo@hmcts.net>
2021-02-02 07:49:23 +0000

    uppercase script params

--- update-json.sh
+++ update-json.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-file="definitions/appeal/json/AuthorisationCaseField.json"
-lineWithRoleToCopyFrom="${1:-caseworker-ia-caseofficer}"
-newRole="${2:-caseworker-wa-task-configuration}"
-output="${3:-output.json}"
+FILE="definitions/appeal/json/AuthorisationCaseField.json"
+LINE_WITH_STRING_TO_COPY_FROM="${1:-caseworker-ia-caseofficer}"
+NEW_ROLE="${2:-caseworker-wa-task-configuration}"
+OUTPUT="${3:-output.json}"
 
 while IFS= read -r line
 do
   echo "$line"
-  echo "$line" >> ${output}
-  if [[ $line =~ "${lineWithRoleToCopyFrom}" ]] 
+  echo "$line" >> ${OUTPUT}
+  if [[ $line =~ "${LINE_WITH_STRING_TO_COPY_FROM}" ]] 
   then
-    echo "${line/$lineWithRoleToCopyFrom/$newRole}" >> ${output}
+    echo "${line/$LINE_WITH_STRING_TO_COPY_FROM/$NEW_ROLE}" >> ${OUTPUT}
   fi
 
-done < "$file"
+done < "$FILE"
