be98dd7b444956eb7abb47e483d7ba54d82ae96e
kalachandrasekar1 <114995593+kalachandrasekar1@users.noreply.github.com>
2023-06-02 12:10:41 +0100

    Ria 6679 upgrade yarn3 (#989)
    Co-authored-by: filippo-possenti-version1 <109352283+filippo-possenti-version1@users.noreply.github.com>

---
+++ bin/generate-json.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+pushd ccd-definition-processor yarn xlsx2json -D ../definitions/appeal/json -i ../definitions/appeal/xlsx/ccd-appeal-config-base.xlsx && popd && pretty-quick --pattern 'appeal/**/json/**.json' && sed -e 's+http://ia-case-api:8090+${CCD_DEF_IA_URL}+g' ./definitions/appeal/json/CaseEvent.json  > ./definitions/appeal/json/CaseEvent.json.new && mv ./definitions/appeal/json/CaseEvent.json.new ./definitions/appeal/json/CaseEvent.json && mv definitions/appeal/json/UserProfile.json definitions/appeal/env/${CCD_ENV:-dev}/UserProfile.json
