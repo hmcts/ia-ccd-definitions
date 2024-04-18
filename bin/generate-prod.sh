c3b646b962dcce7b52790e46acf42bab9e388860
Andrey Pereverzin <andrey.pereverzin1@hmcts.net>
2024-04-18 11:45:32 +0100

    Added tempPreviousRemissionDetails field

--- bin/generate-prod.sh
+++ bin/generate-prod.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 source default.env
-CCD_ENV=prod yarn copy-json && yarn decrypt && CCD_DEF_VERSION='' CCD_ENV=prod CCD_DEF_IA_URL=$npm_package_config_prod_iaCaseUrl CCD_DEF_AAC_URL=$npm_package_config_prod_aacUrl &&  yarn generate-excel
+CCD_ENV=prod yarn copy-json && yarn decrypt && CCD_DEF_VERSION='' CCD_ENV=prod CCD_DEF_IA_URL=$npm_package_config_prod_iaCaseUrl CCD_DEF_AAC_URL=$npm_package_config_prod_aacUrl yarn generate-excel
