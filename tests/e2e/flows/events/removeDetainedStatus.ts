56e3ea8948c1d2e7d71aa35955179915535814f8
elizereyes123 <107991381+elizereyes123@users.noreply.github.com>
2025-06-27 14:22:41 +0100

    - Add Legally Represented Manual test - Add Legally Represented Manual - Remove detained status test

--- tests/e2e/flows/events/removeDetainedStatus.ts
+++ tests/e2e/flows/events/removeDetainedStatus.ts
@@ -6,6 +6,7 @@ const { I } = inject();
 
 class RemoveDetainedStatus {
 
+    //Legally Represented
     async removeStatus(contactPreference: 'Email' | 'Text' = 'Email') {
         await this.detentionRemovalDetails();
         await this.appellantAddress('Yes');
@@ -13,6 +14,20 @@ class RemoveDetainedStatus {
         await this.removeDetainedCYA();
         await I.clickButtonOrLink('Remove Detained Status');
         await this.removeDetainedConfirmation();
+    }
+
+    //Legally Represented - Manual
+    async removeStatusAiPNo() {
+        await this.detentionRemovalDetails();
+        await this.appellantAddressManual();
+        await this.appellantContactDetails();
+        await this.removeDetainedManualCYA();
+        await I.clickButtonOrLink('Remove Detained Status');
+        await this.removeDetainedConfirmation();
+    }
+
+    async validateIsNonDetained() {
+        await this.checkAppealTabDetentionNo();
     }
 
     async detentionRemovalDetails() {
@@ -40,6 +55,16 @@ class RemoveDetainedStatus {
         await I.clickContinue();
     }
 
+    async appellantAddressManual() {
+        await I.waitForElement('#appellantAddress_appellantAddress_postcodeInput');
+        await I.fillField('#appellantAddress_appellantAddress_postcodeInput', appellant.address.postcode);
+        await I.click('//*[@id="appellantAddress_appellantAddress_postcodeLookup"]/button');
+        await I.waitForVisible('#appellantAddress_appellantAddress_addressList',60);
+        await I.waitForText('1 address found', 60);
+        await I.selectOption('#appellantAddress_appellantAddress_addressList', appellant.address.addressLine1 + ', ' + appellant.address.postTown);
+        await I.clickContinue();
+    }
+
     async appellantContactPreference(preference: 'Email' | 'Text') {
         await I.waitForText('The appellant\'s contact preference', 60);
         await I.click(`#contactPreference-wants${preference}`);
@@ -56,6 +81,12 @@ class RemoveDetainedStatus {
         await I.clickContinue();
     }
 
+    async appellantContactDetails() {
+        await I.fillField('#internalAppellantMobileNumber', appellant.mobile);
+        await I.fillField('#internalAppellantEmail', appellant.email);
+        await I.clickContinue();
+    }
+
     async removeDetainedCYA() {
         await I.waitForText('Remove Detained Status', 60);
         await I.waitForText('Check your answers', 60);
@@ -64,6 +95,15 @@ class RemoveDetainedStatus {
         await I.waitForText('Does the appellant have a fixed address?', 60);
         await I.waitForText('Address', 60);
         await I.waitForText('Communication Preference', 60);
+    }
+
+    async removeDetainedManualCYA() {
+        await I.waitForText('Remove Detained Status', 60);
+        await I.waitForText('Check your answers', 60);
+        await I.waitForText('When was the appellant removed from detention?', 60);
+        await I.waitForText('Address', 60);
+        await I.waitForText('Mobile number', 60);
+        await I.waitForText('Email address', 60);
     }
 
     async removeDetainedConfirmation() {
@@ -77,9 +117,6 @@ class RemoveDetainedStatus {
         await I.expectEqual(appellantInDetention, 'No', `The Detention Flag: ${appellantInDetention} on the Appeal Tab is invalid. It should be: No`);
     }
 
-    async validateIsNonDetained() {
-        await this.checkAppealTabDetentionNo();
-    }
 
 
 }
