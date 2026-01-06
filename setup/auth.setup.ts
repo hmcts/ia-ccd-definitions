ffff947e60a450a04d3f3d48c3f90d23c9397577
Douglas Rice <douglas.rice@hmcts.net>
2026-01-06 13:05:47 +0000

    storage state

--- setup/auth.setup.ts
+++ setup/auth.setup.ts
@@ -1,11 +1,11 @@
 import { test as setup, expect } from "@playwright/test";
 import {UserCredentials} from "../tests/e2e/page-objects/pages/idam.po";
-const authFile = "./.auth/authentication.json";
-setup("Authentication", async ({ page }) => {
 
+setup("Authenticate Admin", async ({ page }) => {
+    const authFile = "./.auth/LegalAdminOfficer.json";
     let userName: string = 'CRD_func_test_aat_adm66@justice.gov.uk';
     let password: string = 'AldgateT0wer';
-console.log('RUNNING SETUP');
+console.log('RUNNING SETUP1');
     // Navigate to login page
     await page.goto('https://xui-ia-case-api-pr-2887.preview.platform.hmcts.net');
 
@@ -16,6 +16,26 @@ console.log('RUNNING SETUP');
 
     // Assertion: Verify login success
     await expect(page.locator('//*[@id="content"]/h3')).toContainText('My work')
+
+// Save authentication state
+    await page.context().storageState({ path: authFile });
+});
+
+setup("Authenticate Legal Rep", async ({ page }) => {
+    const authFile = "./.auth/LegalRepresentative.json";
+    let userName: string = 'ialegalreporgcreator12@mailnesia.com';
+    let password: string = 'Aldg@teT0wer';
+    console.log('RUNNING SETUP2');
+    // Navigate to login page
+    await page.goto('https://xui-ia-case-api-pr-2887.preview.platform.hmcts.net');
+
+    // Perform login
+    await page.locator("#username").fill(userName);
+    await page.locator("#password").fill(password);
+    await page.locator('[name="save"]').click();
+
+    // Assertion: Verify login success
+    await expect(page.locator('//*[@id="content"]/div/h1')).toContainText('Case list')
 
 // Save authentication state
     await page.context().storageState({ path: authFile });
