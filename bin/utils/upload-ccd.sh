1bf821a156573712c5a47052d581904ef429f221
Mike Brown <michael.brown@hmcts.net>
2026-02-13 16:52:10 +0000

    CCD_URL hardcoded to 2620

--- bin/utils/upload-ccd.sh
+++ bin/utils/upload-ccd.sh
@@ -66,7 +66,8 @@ case $ENV in
   preview)
     echo "inside ENV preview"
     FILENAME="ccd-appeal-config-preview-pr${PR_NUMBER}.xlsx"
-    CCD_URL="https://ccd-definition-store-ia-case-api-pr-${PR_NUMBER}.preview.platform.hmcts.net"
+    #CCD_URL="https://ccd-definition-store-ia-case-api-pr-${PR_NUMBER}.preview.platform.hmcts.net"
+    CCD_URL="https://ccd-definition-store-ia-case-api-pr-2620.preview.platform.hmcts.net"
     GENERATE_CMD="corepack yarn generate -e preview -p ${PR_NUMBER}"
     TOKEN_ENV="aat"
     ;;
